06e3a164f4ef2b6abb5ad58bc7381eafad4ad931
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-22 18:44:29 +0500

    commands added

---
+++ JS-Assignments/Assignment-1/app.js
@@ -0,0 +1,6 @@
+alert("Hello! JS.");
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land.. \nHappy Coding!");
+
+
+console.log("Hello... I can run JS through my browser's console")
