6b07f8c4bddbe37c067a73044aaabcbf433b7fa7
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-22 23:13:07 +0500

    commands added

---
+++ JS-Assignments/Chapter-2/app.js
@@ -0,0 +1,30 @@
+var userName = "Shah Jamra";
+console.log(userName);
+
+var message = "Hello World";
+console.log(message);
+alert(message);
+
+var myName = "Shahzaib Ali Jamro";
+console.log(myName);
+alert(myName);
+
+var age = "15 Years Old";
+console.log(age);
+alert(age);
+
+var profession = "Certified Mern Stack Developer";
+console.log(profession);
+alert(profession);
+
+var pizza ="PIZZA \nPIZZ \nPIZ \nPI \nP";
+console.log(pizza);
+alert(pizza)
+
+var email ="shahzaibalijamro@gmail.com";
+console.log(email);
+alert("My email address is" +" "+ email)
+
+var book ='"A smarter way to learn JavaScript"';
+console.log(book);
+alert("I am trying to learn from the book" +" \n"+ book)
