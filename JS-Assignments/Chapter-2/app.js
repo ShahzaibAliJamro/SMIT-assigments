3d2b579a74eddcda9b440c28644dc5769053ad6b
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-23 19:12:23 +0500

    commands added

--- JS-Assignments/Chapter-2/app.js
+++ JS-Assignments/Chapter-2/app.js
@@ -5,17 +5,23 @@ var message = "Hello World";
 console.log(message);
 alert(message);
 
-var myName = "Shahzaib Ali Jamro";
-console.log(myName);
-alert(myName);
+// var myName = "Shahzaib Ali Jamro";
+// console.log(myName);
+// alert(myName);
 
-var age = "15 Years Old";
-console.log(age);
-alert(age);
+// var age = "15 Years Old";
+// console.log(age);
+// alert(age);
 
-var profession = "Certified Mern Stack Developer";
-console.log(profession);
-alert(profession);
+// var profession = "Certified Mern Stack Developer";
+// console.log(profession);
+// alert(profession);
+
+
+var fullName = prompt("Enter Your Full Name !")
+var age = prompt("Enter Your Age !")
+var profession = prompt("Enter Your profession !")
+alert("Your full name is " + fullName + " , your age is " + age + " and you are a " + profession)
 
 var pizza ="PIZZA \nPIZZ \nPIZ \nPI \nP";
 console.log(pizza);
@@ -28,3 +34,5 @@ alert("My email address is" +" "+ email)
 var book ='"A smarter way to learn JavaScript"';
 console.log(book);
 alert("I am trying to learn from the book" +" \n"+ book)
+
+document.write("<h1>Yup! I can write HTML using Javascript</h1>")
