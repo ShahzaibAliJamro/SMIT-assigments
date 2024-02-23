99fff0456900edf5471982c7187c0105ede7faef
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-24 00:28:40 +0500

    commands added

--- JS-Assignments/Chapter-3/app.js
+++ JS-Assignments/Chapter-3/app.js
@@ -1,3 +1,17 @@
 var myAge = 17;
 console.log(myAge);
 alert("I am " + myAge +" years old.");
+
+
+var birthYear = 2006;
+
+document.write("My birth year is " + birthYear + "\n" + "Data type of my declared variable is " + typeof(birthYear));
+
+
+
+var visitorName = prompt("Enter your name.");
+var productTitle = prompt("What do you want to purchase?");
+var quantity = prompt("Explain the quantity");
+var storeName = ("daraz.pk");
+
+document.write("\n" + visitorName + " ordered" + " " + quantity + " " + productTitle + " on " + storeName)
