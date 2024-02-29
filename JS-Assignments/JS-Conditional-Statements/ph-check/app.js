9006e9c231ae9e32fec4b3158d8439b3e61c32c5
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-02-29 14:59:34 +0500

    commands added

---
+++ JS-Assignments/JS-Conditional-Statements/ph-check/app.js
@@ -0,0 +1,25 @@
+var pH = +prompt("Enter a pH value from 1 to 14!")
+
+if (pH > 14){
+    document.write("Invalid pH value described.")
+}else if (pH == 14){
+    document.write("ph level is basic\nex: Concentrated solutions of alkali.")
+}else if (pH == 13){
+    document.write("ph level is neutral.")
+}else if (pH == 7){
+    document.write("ph level is neutral.")
+}else if (pH == 7){
+    document.write("ph level is neutral.")
+}else if (pH == 7){
+    document.write("ph level is neutral.")
+}else if (pH == 7){
+    document.write("ph level is neutral.")
+}else if (pH == 7){
+    document.write("ph level is neutral.")
+}else if (pH > 7){
+    document.write("ph level is basic.")
+}else if (pH <= 6){
+    document.write("ph level is acidic.")
+}else{
+    document.write(pH)
+}
