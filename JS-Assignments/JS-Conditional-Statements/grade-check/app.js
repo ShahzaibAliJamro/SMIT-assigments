140f26f89ee72ebd9545a6cfa8ab74a657f91fcc
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-01 07:12:50 +0500

    commands added

--- JS-Assignments/JS-Conditional-Statements/grade-check/app.js
+++ JS-Assignments/JS-Conditional-Statements/grade-check/app.js
@@ -4,19 +4,19 @@ if(percentage > 100){
     document.write("Invalid Percentage !!");
 }else if(percentage <= 33){
     document.write("You are a failure!");
-}else if(percentage <= 40 && percentage > 33){
+}else if(percentage <= 39 && percentage > 32){
     document.write("E Grade!");
-}else if(percentage <= 50 && percentage > 40){
+}else if(percentage <= 49 && percentage > 39){
     document.write("D Grade!");
-}else if(percentage <= 60 && percentage > 51){
+}else if(percentage <= 59 && percentage > 49){
     document.write("C Grade!");
-}else if(percentage <= 70 && percentage > 61){
+}else if(percentage <= 69 && percentage > 59){
     document.write("B Grade!");
-}else if(percentage <= 80 && percentage > 71){
+}else if(percentage <= 79 && percentage > 69){
     document.write("Congratulations, You've got an A Grade!");
-}else if(percentage <= 90 && percentage > 81){
+}else if(percentage <= 89 && percentage > 79){
     document.write("Congratulations, You've got an A+ Grade!");
-}else if(percentage <= 100 && percentage > 91){
+}else if(percentage <= 100 && percentage > 89){
     document.write("Congratulations, You've got an A++ Grade!");
 }else{
     document.write(percentage);
