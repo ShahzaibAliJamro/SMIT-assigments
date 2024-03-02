0108cae891dd666dd854e4993decde43aeb4acf7
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-03 00:04:53 +0500

    commands added

---
+++ JS-Assignments/JS-Conditional-Statements/Q7/app.js
@@ -0,0 +1,10 @@
+var time = +prompt('What time is it now?');
+if(time >= 0 && time < 1200){
+    alert('Good Morning');
+}else if (time >= 1200 && time < 1700){
+    alert('Good Afternoon');
+}else if (time >= 1700 && time < 2100){
+    alert('Good Evening');
+}else if (time >= 2100 && time < 2400){
+    alert('Good Night');
+}
