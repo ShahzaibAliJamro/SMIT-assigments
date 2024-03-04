fcde9b2d1163b26f221ecca42699cd50a803bc12
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-04 18:49:33 +0500

    commands added

--- JS-Assignments/JS-Conditional-Statements/Q6/app.js
+++ JS-Assignments/JS-Conditional-Statements/Q6/app.js
@@ -1,10 +1,12 @@
 var num1 = +prompt('Enter a variable');
 var operator = prompt('What do you want to perform?\nex: + , - , * , / , % .');
 var num2 = +prompt('Enter second variable');
+var num3 = num1 + num2
+var num4 = num1 - num2
 if(operator === '+'){
-    alert('The answer is ' + num1 + num2 )
+    alert('The answer is ' + num3 )
 }else if(operator === '-'){
-    alert('The answer is ' + num1 - num2 )
+    alert('The answer is ' + num4 )
 }else if(operator === '*'){
     alert('The answer is ' + num1 * num2 )
 }else if(operator === '/'){
