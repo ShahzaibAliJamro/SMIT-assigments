a5132d48cf767d340f9f585b9138bf8d9ea9e0fe
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-04 18:44:56 +0500

    commands added

--- JS-Assignments/JS-Conditional-Statements/Q9/app.js
+++ JS-Assignments/JS-Conditional-Statements/Q9/app.js
@@ -1,4 +1,4 @@
-var character = prompt('Enter a character and I will tell you if it is a number, uppercase letter or lowercase letter!');
+var character = prompt('Enter a character and I will tell you if it is a number, or String!');
 if(!isNaN(character)){
     alert(character + ' is a number.')
 }else{
