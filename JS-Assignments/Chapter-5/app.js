a07e6d0ee9fc8aa5a14150584dcefa685280d0e2
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-09 02:57:49 +0500

    commands added

---
+++ JS-Assignments/Chapter-5/app.js
@@ -0,0 +1,27 @@
+// const num1 = +prompt(`Enter a number`);
+// const num2 = +prompt(`Enter another number`);
+// document.write(`Sum of <b>${num1}</b> and <b>${num2}</b> is <b>${num1+num2}</b>`);
+
+
+// const num3 = +prompt(`Enter a number`);
+// const num4 = +prompt(`Enter another number`);
+// document.write(`Subtraction of <b>${num3}</b> and <b>${num4}</b> is <b>${num3-num4}</b>`);
+
+
+// const num5 = +prompt(`Enter a number`);
+// const num6 = +prompt(`Enter another number`);
+// document.write(`Product of <b>${num5}</b> and <b>${num6}</b> is <b>${num5*num6}</b>`)
+
+
+// const num7 = +prompt(`Enter a number`);
+// const num8 = +prompt(`Enter another number`);
+// document.write(`Division of <b>${num7}</b> and <b>${num8}</b> is <b>${num7/num8}</b>`);
+
+
+
+// const num9 = +prompt(`Enter a number`);
+// const num10 = +prompt(`Enter another number`);
+// document.write(`modulus of <b>${num9}</b> and <b>${num10}</b> is <b>${num9%num10}</b>`);
+
+
+
