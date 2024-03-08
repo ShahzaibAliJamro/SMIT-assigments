45b7ac8ff5a840bbe091ccdfb9f009b5d59352c9
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-09 01:50:45 +0500

    commands added

--- JS-Assignments/Chapter-3/app.js
+++ JS-Assignments/Chapter-3/app.js
@@ -5,13 +5,11 @@ alert("I am " + myAge +" years old.");
 
 var birthYear = 2006;
 
-document.write("My birth year is " + birthYear + "\n" + "Data type of my declared variable is " + typeof(birthYear));
-
-
+document.write(`My birth year is <b>${birthYear}</b> Data type of my declared variable is ${typeof(birthYear)}.`);
 
 var visitorName = prompt("Enter your name.");
 var productTitle = prompt("What do you want to purchase?");
 var quantity = prompt("Explain the quantity");
 var storeName = ("daraz.pk");
 
-document.write("\n" + visitorName + " ordered" + " " + quantity + " " + productTitle + " on " + storeName)
+document.write(`<br> <b>${visitorName}</b> ordered <b>${quantity}</b> <b>${productTitle}</b> on <b>${storeName}</b>`)
