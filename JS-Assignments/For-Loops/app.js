460131a7da386ce5c022cad10d57d865af8a988e
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-16 20:29:10 +0500

    commands added

---
+++ JS-Assignments/For-Loops/app.js
@@ -0,0 +1,74 @@
+//              Q1 Start
+
+
+// for (let i = 1; i <= 5; i++) {
+//     document.write('Hello World<br>');
+// }
+
+
+//              Q1 End
+
+
+
+
+
+
+
+
+//             Q2 Start
+
+
+// for (let i = 1; i <= 10; i++) {
+//     document.write(`${i}<br>`)
+// }
+
+
+//              Q2 End
+
+
+
+
+
+
+
+
+//              Q3 Start
+
+
+// const number = +prompt("Enter a number and I'll print it's table!")
+// const length = +prompt('Enter the length!')
+// for (let i = 1; i <= length; i++) {
+//     document.write(`${number} * ${i} = ${number*i}<br>`);
+// }
+
+
+//               Q3 End
+
+
+
+
+
+
+
+
+//              Q4 Start
+
+
+let phoneCompanies = ['Nokia','Samsung','Apple','Sony','Huawei'];
+
+for (let i = 0; i < phoneCompanies.length; i++) {
+    document.write(`${phoneCompanies[i]}<br>`);
+}
+
+
+//               Q4 End
+
+
+
+
+
+
+
+//               Q5 Start
+
+
