bf7463b8d704f2d22cbdc7ef83522a9e455a216c
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-26 23:41:36 +0500

    commands added

--- JS-Assignments/JS-Strings/app.js
+++ JS-Assignments/JS-Strings/app.js
@@ -6,18 +6,27 @@
 
 
 
-//       Q1
+//       Q2
 
 // document.write(`
-// <input type="text" class="input" placeholder="enter your favorite smartphone name">
-// <button onclick="click()">Add</button>
+// <input type="text" placeholder="enter your favorite smartphone name" id="input">
+// <button onclick="click1()">Add</button>
 //     <h1 class="h1"></h1>
 //     <h1 class="h2"></h1>
 // `)
-let input = document.querySelector('.input');
-let h1 = document.querySelector('.h1');
-let h2 = document.querySelector('.h2');
+// let input = document.querySelector('#input');
+// let h1 = document.querySelector('.h1');
+// let h2 = document.querySelector('.h2');
 
-function click() {
-    console.log(input.value);
-}
+// function click1() {
+//     // console.log(input.value);
+//     h1.innerHTML = `My favorite phone model is ${input.value}`;
+//     h2.innerHTML = `Length of string is ${h1.innerHTML.length}`
+// }
+
+
+
+//       Q3
+
+let string = 'Pakistani';
+console.log(string.indexOf('n'));
