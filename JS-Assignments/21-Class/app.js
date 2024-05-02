841a5a40ecd4ffdad572569aceceb609ee4fcaab
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-05-02 17:10:12 +0500

    commands added

--- JS-Assignments/21-Class/app.js
+++ JS-Assignments/21-Class/app.js
@@ -19,18 +19,48 @@
 // object.lastName = 'Ali';
 // console.log(object);
 
-let username = document.querySelector('.input-name');
-let userEmail = document.querySelector('.input-email');
-let userAge = document.querySelector('.input-age');
-let userCity = document.querySelector('.input-city');
-let user;
+// let username = document.querySelector('.input-name');
+// let userEmail = document.querySelector('.input-email');
+// let userAge = document.querySelector('.input-age');
+// let userCity = document.querySelector('.input-city');
+// let user;
 
-function getObject() {
-    user = {
-        name: username.value,
-        email: userEmail.value,
-        age: userAge.value,
-        city: userCity.value,
-    }
-    console.log(user);
+// function getObject() {
+//     user = {
+//         name: username.value,
+//         email: userEmail.value,
+//         age: userAge.value,
+//         city: userCity.value,
+//     }
+//     console.log(user);
+// }
+
+
+const players = {
+    goatLevelPlayers: ['Messi', 'Ronaldo', 'Pele', 'Maradona'],
+    elite: {
+        top: ['Zidane', 'Xavi', 'Maldini', 'Iniesta', 'Modric', 'Kroos', 'KDB'],
+        normal: {
+            top: ['Beckham', 'Pogba', 'Schweinsteiger'],
+            normal: {
+                extra: {
+                    level: [1,2,3,4,5,6,7,8,9],
+                    low: {
+                        level: {
+                            op: [1,2,3,4,10]
+                        }
+                    }
+                }
+            }
+        },
+    high: {
+        top:["Delio Onnis","Bernard Lacombe","Jean-Pierre Papin","Roger Courtois","Hervé Revelli",  "Thadée Cisowski","Carlos Bianchi","Zlatan Ibrahimović","Dominique Rocheteau","Didier Drogba","Edinson Cavani","Sonny Anderson","Pauleta","Josip Skoblar","George Weah",],
+        }
+    },
+    failed: [{failedTop: [{no1: 'Neymar',no2: 'Ronaldinho',no3: 'Hazard',},
+            {failedNot: [{no1: 'Messi',no2: 'Ronaldo',}]}]
+    }]
 }
+console.log(players.failed[0]);
+console.log(players.failed[0].failedTop[1]);
+console.log(players.failed[0].failedTop[1].failedNot[0].no2);
